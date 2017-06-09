2e22b26f375e54383f5b9b084efff2fb8c80a404
elliotAlderson <marco.caballero.d@gmail.com>
2017-06-09 05:34:28 +0200

    Adding elasticheader directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ElasticHeaderDirective } from '../directives/elastic-header/elastic-header';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ElasticHeaderDirective
   ],
   imports: [
     BrowserModule,
