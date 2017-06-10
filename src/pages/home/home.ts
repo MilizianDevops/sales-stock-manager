3f5953f621b9c2d5529730195cc155196ef0ac30
elliotAlderson <marco.caballero.d@gmail.com>
2017-06-10 05:36:58 +0200

    Updating home timeline
    Added ps resources

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, PopoverController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public popoverCtrl: PopoverController) {
 
+  }
+
+  presentPopover(event: Event) {
+    // let popover = this.popoverCtrl.create(PopoverPage);
+    // popover.present({ ev: event });
   }
 
 }
